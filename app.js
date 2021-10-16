7c3074f714de7bb6640dbc9f7461d920e02677f6
yutthapichai chaiwong <yutthapichai.c@skanhubrewards.com>
2021-10-16 16:04:03 +0700

    update

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(cors());
 
 app.post("/register", async (req, res) => {
