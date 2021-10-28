10332ca5a176fbeb9ca9f1a9513b5ef8c3254317
yutthapichai chaiwong <yutthapichai.c@skanhubrewards.com>
2021-10-28 14:30:46 +0700

    update

--- model/company.js
+++ model/company.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const companySchema = new mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   c_name: { type: String, default: null },
-  c_description: { type: String, default: bull },
+  c_description: { type: String, default: null },
   c_address: { type: String, default: null },
   c_board: [
     {
